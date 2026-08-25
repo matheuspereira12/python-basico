b1c0914d4eca5789dd73a996f2d0829ce8116bca
Matheus Pereira <matheust36@gmail.com>
2026-08-25 10:23:21 -0300

    Exercício Python #003 - Somando Dois Números
    Solicita dois números ao usuário, realiza a soma e exibe o resultado.

---
+++ exercises/ex003_somando_dois_numeros.prompt.py
@@ -0,0 +1,6 @@
+# Soma dois números e exibe o resultado.
+
+num1 = int(input("Digite o primeiro número:\n"))
+num2 = int(input("Digite o segundo número:\n"))
+soma = num1 + num2
+print("A soma entre {} e {} é {}.\n".format(num1, num2, soma))
