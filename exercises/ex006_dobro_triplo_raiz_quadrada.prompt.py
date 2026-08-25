c92571dc9111bff3bb4fce424d733e517928d4a6
Matheus Pereira <matheust36@gmail.com>
2026-08-25 13:55:33 -0300

    Exercício Python #006 - Dobro, Triplo, Raiz Quadrada
    Adiciona o exercício que recebe um número e exibe seu dobro, triplo e raiz quadrada.

---
+++ exercises/ex006_dobro_triplo_raiz_quadrada.prompt.py
@@ -0,0 +1,4 @@
+# Calcula e exibe o dobro, o triplo e a raiz quadrada do valor informado pelo usuário.
+
+num = int(input("Digite qualquer valor:\n"))
+print("O dobro de {} é {}.\nO triplo é {}.\nA raiz quadrada é {}.\n".format(num, (num * 2), (num * 3), (num ** 0.5)))
