4ce074366c07e065eb149056e047c0ac0d278c5c
Matheus Pereira <matheust36@gmail.com>
2026-08-25 13:25:02 -0300

    Exercício Python #005 - Antecessor e Sucessor
    Calcula e exibe o antecessor e o sucessor do número informado pelo usuário.

---
+++ exercises/ex005_antecessor_sucessor.prompt.py
@@ -0,0 +1,4 @@
+# Calcula e exibe o antecessor e o sucessor do número informado pelo usuário.
+
+num = int(input("Digite um número:\n"))
+print("O antecessor de {} é {} e o sucessor é {}.\n".format(num, num - 1, num + 1))
