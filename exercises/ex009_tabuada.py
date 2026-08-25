49ec1649037f04fa8f4d24aeb9abb836abe72817
Matheus Pereira <matheust36@gmail.com>
2026-08-25 16:37:39 -0300

    Exercício Python #009 - Tabuada
    Recebe um número e exibe sua tabuada de 1 a 9.

---
+++ exercises/ex009_tabuada.py
@@ -0,0 +1,4 @@
+# Recebe um número e exibe sua tabuada de 1 a 9.
+
+valor = int(input("Digite um número para ver a tabuada:\n"))
+print("{:>5}\n{:>5}\n{:>5}\n{:>5}\n{:>5}\n{:>5}\n{:>5}\n{:>5}\n{:>5}\n".format(valor * 1, valor * 2, valor * 3, valor * 4, valor * 5, valor * 6, valor * 7, valor * 8, valor * 9))
