3b92cc14b2f0327586fdc29e7ce730e2353aa52c
Matheus Pereira <matheust36@gmail.com>
2026-08-26 10:26:43 -0300

    Exercício Python #012 - Calculando Descontos
    Calcula o valor final de um produto aplicando 5% de desconto.

---
+++ exercises/ex012_calculando_descontos.py
@@ -0,0 +1,5 @@
+# Calcula o valor final do produto aplicando 5% de desconto
+
+valor_produto = float(input("Digite o valor do produto: R$ "))
+desconto = valor_produto * 0.95
+print("O valor final com desconto é: R$ {:.2f}".format(desconto))
