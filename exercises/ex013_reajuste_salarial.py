e3a0df2a8e6a2cd3d193cfa8e3d3d6df90d480e2
Matheus Pereira <matheust36@gmail.com>
2026-08-26 10:43:06 -0300

    Exercício Python #013 - Reajuste Salarial
    Calcula o salário de um funcionário aplicando um reajuste de 15%.

---
+++ exercises/ex013_reajuste_salarial.py
@@ -0,0 +1,5 @@
+# Calcula o salário com reajuste de 15%
+
+salario = float(input("Digite o salário do funcionário: R$ "))
+salario_reajustado = salario * 1.15
+print("O salário com reajuste de 15% será R$ {:.2f}".format(salario_reajustado))
