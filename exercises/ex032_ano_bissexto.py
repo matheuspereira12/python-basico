6322117057f92eb60f232a7fdcf50774c44758e2
Matheus Pereira <matheust36@gmail.com>
2026-08-28 15:21:31 -0300

    Exercício Python #033 - Maior e menor valores
    Verifica e exibe o maior e o menor entre três números informados pelo usuário.

--- exercises/ex032_ano_bissexto.py
+++ exercises/ex032_ano_bissexto.py
@@ -6,6 +6,7 @@ ano = int(input("Digite o ano que deseja analisar. Para analisar o ano atual, di
 if ano == 0:
     
     ano = date.today().year
+    
 if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0):
     print("O ano {} é bissexto.".format(ano))
 else:
---
+++ exercises/ex033_maior_menor_valores.py
@@ -0,0 +1,19 @@
+# Verifica e exibe o menor e o maior entre os três números informados.
+
+
+numero_1 = int(input("Digite o primeiro número: "))
+numero_2 = int(input("Digite o segundo número: "))
+numero_3 = int(input("Digite o terceiro número: "))
+
+if numero_2 < numero_1 and numero_2 < numero_3:
+    print("O menor número é: {}".format(numero_2))
+elif numero_3 < numero_1 and numero_3 < numero_2:
+    print("O menor número é: {}".format(numero_3))
+else:
+    print("O menor número é: {}".format(numero_1))
+if numero_2 > numero_1 and numero_2 > numero_3:
+    print("O maior número é: {}".format(numero_2))
+elif numero_3 > numero_1 and numero_3 > numero_2:
+    print("O maior número é: {}".format(numero_3))
+else:
+        print("O maior número é: {}".format(numero_1))
