7876e8f771ebfc79ff68547801cfdbba7ff8f20d
Matheus Pereira da Silva <143643655+matheuspereira12@users.noreply.github.com>
2026-09-01 15:07:28 -0300

    Exercício Python #040 - Aquele clássico da Média v1.0
    Calcula a média de duas notas e informa a situação do aluno.

---
+++ exercises/ex040_media.py
@@ -0,0 +1,18 @@
+
+
+# Calcula a média das duas notas e informa a situação do aluno.
+
+
+# nota_1 = float(input("Digite a primeira nota do aluno: "))
+
+nota_2 = float(input("Digite a segunda nota do aluno: "))
+
+media = (nota_1 + nota_2) / 2
+
+if media < 5:
+    print("A média do aluno foi {}.\nSituação: reprovado.".format(media))
+elif media < 7:
+    print("A média do aluno foi {}.\nSituação: recuperação.".format(media))
+else:
+    print("A média do aluno foi {}.\nSituação: aprovado!".format(media))
+    
